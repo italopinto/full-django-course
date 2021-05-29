faf2f413da41f8fc13e38226a5b85786efb28b74
italo <italosp00@gmail.com>
2021-05-29 20:54:09 -0300

    Removing digital ocean configs. Removed the os.getenv, because now i'm using python-decouple pack.

--- clients/settings.py
+++ clients/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY') or os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool) or os.getenv('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-try:
-    ALLOWED_HOSTS = list(os.getenv("DJANGO_ALLOWED_HOSTS")) # this is the config of DIGITAL OCEAN
-except TypeError:
-    ALLOWED_HOSTS = ['full-django-course.herokuapp.com', '0.0.0.0']
+ALLOWED_HOSTS = ['full-django-course.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
