abbd36c1ff6d695c8c688af58873456d263bb052
italo <italosp00@gmail.com>
2021-05-29 15:14:11 -0300

    fixed the static root folder to use collect statics

--- clients/settings.py
+++ clients/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "mystatic",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_URL = "/media/"
